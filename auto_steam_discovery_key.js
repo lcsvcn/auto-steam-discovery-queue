571655b7a127cc40a13f46eed9c7f4d6ff2ae452
Lucas V C Nicolau <lcsvcn@users.noreply.github.com>
2018-06-29 22:52:12 -0300

    Update auto_steam_discovery_key.js

--- auto_steam_discovery_key.js
+++ auto_steam_discovery_key.js
@@ -1,16 +1,42 @@
 // ==UserScript==
 // @name         Auto Discovery Queue For Steam
-// @namespace    https://github.com/lcsvcn/auto-steam-discovery-queue/
-// @version      1.0.0
+// @namespace    https://github.com/lcsvcn
+// @downloadURL  https://github.com/lcsvcn/auto-steam-discovery-queue/raw/master/auto_steam_discovery_key.js
+// @version      1.0.1
 // @description  This script will automatically click through your discovery queue so you can get a sale card.
+// @license      MIT License
+// @copyright    Copyright (C) 2018, by Lucas V C Nicolau
 // @author       lcsvcn
 // @match        *://store.steampowered.com/app/*
 // @match        *://store.steampowered.com/agecheck/app/*
 // @match        *://store.steampowered.com/explore/
 // @updateURL    https://github.com/lcsvcn/auto-steam-discovery-queue/raw/master/auto_steam_discovery_key.js
-// @downloadURL  https://github.com/lcsvcn/auto-steam-discovery-queue/raw/master/auto_steam_discovery_key.js
 // @grant        none
 // ==/UserScript==
+
+/**
+ * MIT License
+ *
+ * Copyright (C) 2018, by Lucas V C Nicolau
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 (function() {
     'use strict';
